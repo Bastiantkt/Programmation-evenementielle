470c1e406de942fe4381a8f0d38e369ff85ab808
Bastian <153897299+Bastiantkt@users.noreply.github.com>
2024-12-09 09:26:36 +0100

    Update client.py

--- SAE3.02/CLIENT/client.py
+++ SAE3.02/CLIENT/client.py
@@ -3,15 +3,10 @@ import sys
 from PyQt6 import QtWidgets, QtCore, QtGui
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
 
-def ChargerLeCSS(filename):
-    with open(filename, 'r') as rd:
+def ChargerLeCSS(fichier):
+    with open(fichier, 'r') as rd:
         content = rd.read()
     return content
-
-class ShadowLineEdit(QtWidgets.QLineEdit):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 
 class Interface_Application(QtWidgets.QWidget):
     def __init__(self):
@@ -24,27 +19,27 @@ class Interface_Application(QtWidgets.QWidget):
         self.setWindowTitle('SAE3.02')
         self.setStyleSheet(ChargerLeCSS('main.css'))
         self.ip_label = QtWidgets.QLabel('IP du serveur maitre :')
-        self.ip_input = ShadowLineEdit(self)
+        self.ip_input = QtWidgets.QLineEdit(self)
         self.Ombre(self.ip_input)
         self.ip_input.setText('localhost')
         self.port_label = QtWidgets.QLabel('Port du serveur maitre :')
-        self.port_input = ShadowLineEdit(self)
+        self.port_input = QtWidgets.QLineEdit(self)
         self.Ombre(self.port_input)
         self.port_input.setText('12345')
-        self.send_button = QtWidgets.QPushButton('Envoyer le programme', self)
-        self.send_button.clicked.connect(self.Envoie_Programme)
-        self.Ombre(self.send_button)
-        self.result_text = QtWidgets.QTextEdit(self)
-        self.result_text.setReadOnly(True)
-        self.Ombre(self.result_text)
+        self.envoie_button = QtWidgets.QPushButton('Envoyer le programme', self)
+        self.envoie_button.clicked.connect(self.Envoie_Programme)
+        self.Ombre(self.envoie_button)
+        self.resultat_text = QtWidgets.QTextEdit(self)
+        self.resultat_text.setReadOnly(True)
+        self.Ombre(self.resultat_text)
         grid = QtWidgets.QGridLayout()
         grid.setSpacing(15)
         grid.addWidget(self.ip_label, 1, 0)
         grid.addWidget(self.ip_input, 1, 1)
         grid.addWidget(self.port_label, 2, 0)
         grid.addWidget(self.port_input, 2, 1)
-        grid.addWidget(self.send_button, 3, 0, 1, 2)
-        grid.addWidget(self.result_text, 4, 0, 5, 2)
+        grid.addWidget(self.envoie_button, 3, 0, 1, 2)
+        grid.addWidget(self.resultat_text, 4, 0, 5, 2)
         self.setLayout(grid)
         self.show()
 
@@ -56,47 +51,47 @@ class Interface_Application(QtWidgets.QWidget):
         widget.setGraphicsEffect(shadow)
 
     def Envoie_Programme(self):
-        self.send_button.setEnabled(False)
+        self.envoie_button.setEnabled(False)
 
-        server_ip = self.ip_input.text()
-        server_port = int(self.port_input.text())
+        ip_serveur = self.ip_input.text()
+        port_serveur = int(self.port_input.text())
 
         options = QFileDialog.Option.DontUseNativeDialog
         file_path, _ = QFileDialog.getOpenFileName(self, "Sélectionnez le programme", "", "Tous les fichiers (*)", options=options)
         if not file_path:
-            self.send_button.setEnabled(True)
+            self.envoie_button.setEnabled(True)
             return
 
         try:
             with open(file_path, "rb") as f:
-                program_data = f.read()
+                programme = f.read()
         except Exception as e:
             QMessageBox.critical(self, "Erreur", f"Impossible de lire le fichier : {str(e)}")
-            self.send_button.setEnabled(True)
+            self.envoie_button.setEnabled(True)
             return
 
         import os
-        _, file_extension = os.path.splitext(file_path)
-        language_code = file_extension.lower().strip('.')
+        _, extension_fichier = os.path.splitext(file_path)
+        language_code = extension_fichier.lower().strip('.')
 
         self.thread = QtCore.QThread()
-        self.worker = Worker(server_ip, server_port, program_data, language_code)
+        self.worker = Worker(ip_serveur, port_serveur, programme, language_code)
         self.worker.moveToThread(self.thread)
         self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.on_finished)
-        self.worker.update_result.connect(self.update_result)
+        self.worker.finished.connect(self.arret)
+        self.worker.mettre_a_jour_resultat.connect(self.mettre_a_jour_resultat)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
         self.thread.start()
 
-        self.result_text.clear()
-        self.result_text.append("En attente du résultat...\n")
+        self.resultat_text.clear()
+        self.resultat_text.append("En attente du résultat...\n")
 
-    def update_result(self, data):
-        self.result_text.append(data)
+    def mettre_a_jour_resultat(self, data):
+        self.resultat_text.append(data)
 
-    def on_finished(self):
-        self.send_button.setEnabled(True)
+    def arret(self):
+        self.envoie_button.setEnabled(True)
         self.thread.quit()
         self.thread.wait()
         self.thread = None
@@ -105,42 +100,42 @@ class Interface_Application(QtWidgets.QWidget):
 
 class Worker(QtCore.QObject):
     finished = QtCore.pyqtSignal()
-    update_result = QtCore.pyqtSignal(str)
+    mettre_a_jour_resultat = QtCore.pyqtSignal(str)
 
-    def __init__(self, server_ip, server_port, program_data, language_code):
+    def __init__(self, ip_serveur, port_serveur, programme, language_code):
         super().__init__()
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.program_data = program_data
+        self.ip_serveur = ip_serveur
+        self.port_serveur = port_serveur
+        self.programme = programme
         self.language_code = language_code
 
     def run(self):
         try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((self.server_ip, self.server_port))
+            socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_client.connect((self.ip_serveur, self.port_serveur))
 
-            header = f"{self.language_code}:{len(self.program_data)}".encode()
-            client_socket.sendall(header)
+            header = f"{self.language_code}:{len(self.programme)}".encode()
+            socket_client.sendall(header)
 
-            ack = client_socket.recv(1024).decode()
+            ack = socket_client.recv(1024).decode()
             if ack != "HEADER_RECUE":
                 raise Exception("Problème lors de l'envoi de l'en-tête.")
 
-            client_socket.sendall(self.program_data)
+            socket_client.sendall(self.programme)
 
             result = b''
             while True:
-                data = client_socket.recv(1024)
+                data = socket_client.recv(1024)
                 if not data:
                     break
                 result += data
 
-            self.update_result.emit(result.decode())
-            client_socket.close()
+            self.mettre_a_jour_resultat.emit(result.decode())
+            socket_client.close()
 
         except Exception as e:
-            error_message = f"Une erreur est survenue : {str(e)}"
-            self.update_result.emit(error_message)
+            message_erreur = f"Une erreur est survenue : {str(e)}"
+            self.mettre_a_jour_resultat.emit(message_erreur)
 
         self.finished.emit()
 
