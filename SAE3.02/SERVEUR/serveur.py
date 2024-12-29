11c059ebc63a8b212d57e70689c016e5df63b539
Bastian <153897299+Bastiantkt@users.noreply.github.com>
2024-12-29 16:14:29 +0100

    Update serveur.py

--- SAE3.02/SERVEUR/serveur.py
+++ SAE3.02/SERVEUR/serveur.py
@@ -29,7 +29,7 @@ logging.basicConfig(
 # ------------
 
 if len(sys.argv) < 4:
-    print("Utilisation : python3 serveur.py <port_maitre> <ips_autres> <ports_autres> <max_programmes> <max_cpu_usage> <max_ram_usage>")
+    print("Utilisation : python3 serveur.py <port_maitre> '<ips_autres>,<ips_autres>' '<ports_autres>;<ports_autres>' <max_programmes> <max_cpu_usage> <max_ram_usage>")
     print("Exemple : python3 serveur.py 12345 '127.0.0.1,192.168.1.2' '12346,12347;12348,12349' 2 50 80")
     sys.exit(1)
 
